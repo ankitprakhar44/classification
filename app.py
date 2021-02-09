758a9115d51fda43a9bccba38b76dd665e0663d1
ankitprakhar44 <60107469+ankitprakhar44@users.noreply.github.com>
2021-02-09 20:45:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ from tensorflow import keras
 st.set_option('deprecation.showfileUploaderEncoding', False)
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model=tf.keras.models.load_model(tf.keras.utils.get_file('my_model12.hdf5', origin='https://drive.google.com/u/0/uc?id=1c1uYjTxvx2HP9SSePQ9Zvx-5JSHax-46&export=download'))
+    model=tf.keras.models.load_model(tf.keras.utils.get_file('cymbidium_model.hdf5', origin='https://drive.google.com/u/1/uc?id=1QO8cClJURkUmrHzREvlx0ahnhUMw4n7Q&export=download'))
     return model
 model=load_model()
 st.write("""
-         # Flower Classification
+         # Cymbidium Classification
          """
         )
-file = st.file_uploader("Please upload a flower image", type=["jpg","png","jpeg"])
+file = st.file_uploader("Please upload a image of cymbidium Orchid", type=["jpg","png","jpeg"])
 from PIL import Image, ImageOps
 import numpy as np
 def import_and_predict(image_data, model):
@@ -30,6 +30,6 @@ else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     predictions = import_and_predict(image, model)
-    class_names=['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+    class_names=['Cymbidium acuminatum', 'Cymbidium aliciae', 'Cymbidium aloifolium', 'Cymbidium atropurpureum', 'Cymbidium banaense', 'Cymbidium bicolor', 'Cymbidium borneense', 'Cymbidium canaliculatum', 'Cymbidium changningense', 'Cymbidium chloranthum', 'Cymbidium cochleare', 'Cymbidium concinnum', 'Cymbidium crassifolium', 'Cymbidium cyperifolium', 'Cymbidium daweishanense', 'Cymbidium dayanum', 'Cymbidium defoliatum', 'Cymbidium devonianum', 'Cymbidium eburneum', 'Cymbidium elegans', 'Cymbidium elongatum', 'Cymbidium ensifolium', 'Cymbidium erythraeum', 'Cymbidium erythrostylum', 'Cymbidium faberi', 'Cymbidium finlaysonianum', 'Cymbidium floribundum', 'Cymbidium formosanum', 'Cymbidium gaoligongense', 'Cymbidium goeringii', 'Cymbidium haematodes', 'Cymbidium hartinahianum', 'Cymbidium hookerianum', 'Cymbidium insigne', 'Cymbidium iridioides', 'Cymbidium kanran', 'Cymbidium lancifolium', 'Cymbidium lowianum', 'Cymbidium macrorhizon', 'Cymbidium madidum', 'Cymbidium maguanense', 'Cymbidium mastersii', 'Cymbidium micranthum', 'Cymbidium munronianum', 'Cymbidium nanulum', 'Cymbidium omeiense', 'Cymbidium parishii', 'Cymbidium qiubeiense', 'Cymbidium rectum', 'Cymbidium repens', 'Cymbidium sanderae', 'Cymbidium schroederi', 'Cymbidium seidenfadenii', 'Cymbidium serratum', 'Cymbidium sichuanicum', 'Cymbidium sigmoideum', 'Cymbidium sinense', 'Cymbidium suave', 'Cymbidium suavissimum', 'Cymbidium tamphianum', 'Cymbidium tigrinum', 'Cymbidium tortisepalum', 'Cymbidium tracyanum', 'Cymbidium wadae', 'Cymbidium wenshanense', 'Cymbidium whiteae', 'Cymbidium wilsonii', 'Cymbidium × ballianum', 'Cymbidium × baoshanense', 'Cymbidium × dilatatiphyllum', 'Cymbidium × florinda', 'Cymbidium × gammieanum', 'Cymbidium × glebelandense', 'Cymbidium × hillii', 'Cymbidium × monanthum', 'Cymbidium × nishiuchianum', 'Cymbidium × nomachianum', 'Cymbidium × purpuratum', 'Cymbidium × rosefieldense', 'Cymbidium × woodlandense']
     string="This image most likely is: "+class_names[np.argmax(predictions)]
     st.success(string)
