6eade21f5aea1978b2c5493613b38b66d23ab617
ankitprakhar44 <60107469+ankitprakhar44@users.noreply.github.com>
2021-04-28 13:17:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ file = st.file_uploader("Please upload an image of Cymbidium Orchid", type=["jpg
 from PIL import Image, ImageOps
 import numpy as np
 def import_and_predict(image_data, model):
+    global prediction
 
     size = (180,180)
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     img = np.asarray(image)
     img_reshape = img[np.newaxis,...]
-    global prediction = model.predict(img_reshape)
+    prediction = model.predict(img_reshape)
 
     return prediction
 if file is None:
