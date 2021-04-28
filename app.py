690cc702d372ab277c91c65faba6d89e86fb836a
ankitprakhar44 <60107469+ankitprakhar44@users.noreply.github.com>
2021-04-28 13:22:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ else:
     string1=str(np.argmax(predictions))
     string="It is "+string1+"% likely that his image belongs to the specie: "+class_names[np.argmax(predictions)]
     st.success(string)
-    string2= max_pred
+    string2= max_pred.sort()
     st.success(string2)
     
     
